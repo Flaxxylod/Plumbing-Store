871994c46674f9b7d42b0c9da2481bf0af0f9c82
flaxxylod <vanechka.reva@bk.ru>
2026-04-09 19:06:37 +0300

    ZustandMobileVersionCompleted

--- src/Store/UI_Store.js
+++ src/Store/UI_Store.js
@@ -1,11 +1,35 @@
+
 import { create } from "zustand";
 
-const UI_Store = create((set) => ({
+const UI_Store = create((set, get) => ({
     OpenSearchInput: false,
     OpeningSearchInput: () => set((state) => ({ OpenSearchInput: !state.OpenSearchInput })),
-    reset: () => set({ OpenSearchInput: false })
-})
+    reset: () => set({ OpenSearchInput: false }),
+    MobileVersion_width: 768, // ширина при которой должна включаться мобильная версия
+    CurrentWidth: window.innerWidth, // ширина экрана при загрузке сайта
 
+    MobileVersion: false, //переключатель мобильной версии
+    FindingWidth: () => { // нахождение ширины экрана в настоящий момент
+        set({ CurrentWidth: window.innerWidth }) // обновляет в поле CurrentWidth: значение ширины экрана
+        get().SwitchMobileVersion() //вызов следующей функции
+    },
+
+    SwitchMobileVersion: () => { // функция переключения мобильной версии 
+        if (get().CurrentWidth >= get().MobileVersion_width) { // если ширина больше или равно ширине при которой включается мобильная версия
+            set({ MobileVersion: true }) //мобильная версия активирована
+
+        }
+        else {
+            set({ MobileVersion: false }) //декстопная версия активирована
+        }
+    }
+
+
+
+
+})
 )
+UI_Store.getState().FindingWidth() // вызов функции сразу после загрузки страницы, чтобы сразу включилась либо декстопная либо мобильная версия
+addEventListener("resize", () => UI_Store.getState().FindingWidth()) // вызов функции из стора при изменении ширины сайта
 
 export default UI_Store
