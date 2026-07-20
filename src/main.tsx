cadcb408e00977e1d273f6802bd7c24c8468b7c5
Flaxxylod <vanechka.reva@bk.ru>
2026-07-20 19:38:16 +0300

    fixxingDeploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { AppProvider } from './Context.js'; // Импортируем только провайдер
-import "./Styles/reset.css";
-import "./index.css"
-import "./Styles/common.css";
+import "./Styles/Reset.css";
+import "./Index.css"
+import "./Styles/Common.css";
 import RouterWrapper from './RouterWrapper.jsx'; // Выносим логику роутов в отдельный файл
 
 createRoot(document.getElementById('root')).render(
