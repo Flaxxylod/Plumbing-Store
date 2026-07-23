2667a7ce5c309f029da4e281717ee67988711c1a
Flaxxylod <vanechka.reva@bk.ru>
2026-07-23 14:54:54 +0300

    fixingJSON.

--- src/Landing/main/Ouradvantages/advantagesCards/advantagesData.ts
+++ src/Landing/main/Ouradvantages/advantagesCards/advantagesData.ts
@@ -8,19 +8,19 @@ interface AdvantagesCard {
 export const AdvantagesCardData: AdvantagesCard[] = [
     {
         id: 0,
-        picture: "src/assets/Landing/main/Ouradvantages/Picture1.svg",
+        picture: new URL('./assets/Landing/main/Ouradvantages/Picture1.svg', import.meta.url).href,
         subheader: "Бесплатный возврат",
         text: "Если товар вам не подходит, вы можете верунть его в течении 30 дней.",
     },
     {
         id: 1,
-        picture: "src/assets/Landing/main/Ouradvantages/Picture2.svg",
+        picture: new URL('./assets/Landing/main/Ouradvantages/Picture2.svg', import.meta.url).href,
         subheader: "Гарантия10 лет",
         text: "Стандарты ISO9001 и двойной контроль качества гарантируют долгосрочную безупречную работу изделий.",
     },
     {
         id: 2,
-        picture: "src/assets/Landing/main/Ouradvantages/Picture3.svg",
+        picture: new URL('./assets/Landing/main/Ouradvantages/Picture3.svg', import.meta.url).href,
         subheader: "Безопасная оплата",
         text: "Банковской картой или наличными при получении. ",
     },
